d929d02a64d02fb002c48c807cd1c1c0b78b1af1
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-05-22 02:22:47 +0300

    feat: create Department table

---
+++ src/main/java/com/sustainabilitytracker/sustainabilitytracker/entities/Department.java
@@ -0,0 +1,42 @@
+package com.sustainabilitytracker.sustainabilitytracker.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "departments")
+public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "company_id")
+    private Company company;
+
+    @Column(name = "name")
+    private String name;
+
+    @Lob
+    @Column(name = "description")
+    private String description;
+
+    @ColumnDefault("1")
+    @Column(name = "is_active")
+    private Boolean isActive;
+
+    @ColumnDefault("CURRENT_TIMESTAMP")
+    @Column(name = "created_at")
+    private Instant createdAt;
+
+
+}
