1c84c31673710f5eb45c29523e6c200e8adb1a59
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-05-22 02:00:43 +0300

    feat: create Company

---
+++ src/main/java/com/sustainabilitytracker/sustainabilitytracker/entities/Company.java
@@ -0,0 +1,54 @@
+package com.sustainabilitytracker.sustainabilitytracker.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "companies")
+public class Company {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "industry")
+    private String industry;
+
+    @Column(name = "country")
+    private String country;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "size")
+    private String size;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @ColumnDefault("1")
+    @Column(name = "is_active")
+    private Boolean isActive;
+
+    @ColumnDefault("CURRENT_TIMESTAMP")
+    @Column(name = "created_at")
+    private Instant createdAt;
+
+    @ColumnDefault("CURRENT_TIMESTAMP")
+    @Column(name = "updated_at")
+    private Instant updatedAt;
+
+
+}
