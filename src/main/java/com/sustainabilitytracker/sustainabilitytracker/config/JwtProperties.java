997dd803a56fe3f540936864b596df69224a2f25
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-05-27 22:47:46 +0300

    feat: create class that reads JWT settings from application.properties

--- src/main/java/com/sustainabilitytracker/sustainabilitytracker/config/JwtProperties.java
+++ src/main/java/com/sustainabilitytracker/sustainabilitytracker/config/JwtProperties.java
@@ -1,4 +1,13 @@
 package com.sustainabilitytracker.sustainabilitytracker.config;
 
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "spring.jwt")
 public class JwtProperties {
+    private String secret;
+    private long expiration;
 }
