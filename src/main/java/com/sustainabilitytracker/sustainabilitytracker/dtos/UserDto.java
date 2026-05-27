34c0822e9477c80e41af733e20e4535e9a379922
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-05-28 02:28:34 +0300

    feat: create user dto for simple response

---
+++ src/main/java/com/sustainabilitytracker/sustainabilitytracker/dtos/UserDto.java
@@ -0,0 +1,12 @@
+package com.sustainabilitytracker.sustainabilitytracker.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class UserDto {
+    private Long id;
+    private String name;
+    private String email;;
+}
