b0eabb1be816c0906f97a0647c5d4547dff484de
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-05-28 02:29:56 +0300

    feat: create user response dto

--- src/main/java/com/sustainabilitytracker/sustainabilitytracker/dtos/response/UserResponse.java
+++ src/main/java/com/sustainabilitytracker/sustainabilitytracker/dtos/response/UserResponse.java
@@ -1,4 +1,18 @@
 package com.sustainabilitytracker.sustainabilitytracker.dtos.response;
 
+import lombok.Data;
+import java.time.Instant;
+
+
+@Data
 public class UserResponse {
+    private Long id;
+    private String fullName;
+    private String email;
+    private String role;
+    private String companyName;
+    private String departmentName;
+    private Boolean isActive;
+    private Instant createdAt;
+
 }
