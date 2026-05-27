21c2f0610d0161796a56c7ddc6c150c68000fe80
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-05-27 23:39:59 +0300

    feat: create user controller

---
+++ src/main/java/com/sustainabilitytracker/sustainabilitytracker/controllers/UserController.java
@@ -0,0 +1,11 @@
+package com.sustainabilitytracker.sustainabilitytracker.controllers;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/users")
+public class UserController {
+
+}
