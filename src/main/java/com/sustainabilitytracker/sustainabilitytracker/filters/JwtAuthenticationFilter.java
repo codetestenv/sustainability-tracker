7f56b815ab3ef4d276e6555f9315b80705c757b3
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-05-29 01:12:30 +0300

    feat: create jwt authentication filter to load user and filter the header

--- src/main/java/com/sustainabilitytracker/sustainabilitytracker/filters/JwtAuthenticationFilter.java
+++ src/main/java/com/sustainabilitytracker/sustainabilitytracker/filters/JwtAuthenticationFilter.java
@@ -6,6 +6,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -26,8 +27,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(
             HttpServletRequest request,
-            HttpServletResponse response,
-            FilterChain filterChain
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
     ) throws ServletException, IOException {
 
         // Skip if no Authorization header or not Bearer
