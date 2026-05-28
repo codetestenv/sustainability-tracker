4b97d6f4f6850fda36d46daaf0ec789939aa704f
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-05-28 03:36:43 +0300

    refactor: make user response by proper order

---
+++ src/main/java/com/sustainabilitytracker/sustainabilitytracker/validation/SimpleValidationHandler.java
@@ -0,0 +1,34 @@
+package com.sustainabilitytracker.sustainabilitytracker.validation;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+//@RestControllerAdvice
+public class SimpleValidationHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, Object>> handleValidation(MethodArgumentNotValidException ex) {
+
+        Map<String, String> fieldErrors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .collect(Collectors.toMap(
+                        FieldError::getField,
+                        FieldError::getDefaultMessage
+                ));
+
+        return ResponseEntity.badRequest().body(Map.of(
+                "timestamp", LocalDateTime.now(),
+                "status", 400,
+                "error", "Bad Request",
+                "messages", fieldErrors
+        ));
+    }
+}
