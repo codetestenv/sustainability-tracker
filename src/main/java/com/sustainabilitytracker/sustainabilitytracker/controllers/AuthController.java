cb2482381dbae56cc882faa05d6a39f164bc0061
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-05-29 16:34:38 +0300

    refactor: change the role route routes config

--- src/main/java/com/sustainabilitytracker/sustainabilitytracker/controllers/AuthController.java
+++ src/main/java/com/sustainabilitytracker/sustainabilitytracker/controllers/AuthController.java
@@ -1,6 +1,21 @@
 package com.sustainabilitytracker.sustainabilitytracker.controllers;
 
+import com.sustainabilitytracker.sustainabilitytracker.config.JwtProperties;
+import com.sustainabilitytracker.sustainabilitytracker.dtos.request.LoginRequest;
+import com.sustainabilitytracker.sustainabilitytracker.dtos.response.JwtResponse;
+import com.sustainabilitytracker.sustainabilitytracker.entities.User;
+import com.sustainabilitytracker.sustainabilitytracker.repositories.UserRepository;
+import com.sustainabilitytracker.sustainabilitytracker.security.JwtTokenProvider;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
+import org.springframework.data.repository.Repository;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +23,34 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 @RequestMapping("/auth")
 public class AuthController {
+
+    private final AuthenticationManager authenticationManager;
+    private final UserRepository userRepository;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final JwtProperties jwtProperties;
+
+    @PostMapping("/login")
+    public ResponseEntity<JwtResponse> login(
+            @RequestBody LoginRequest request,
+            HttpServletResponse response){
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        var user = userRepository.findByEmail(request.getEmail()).orElseThrow();
+
+        var accessToken = jwtTokenProvider.generateAccessToken(user);
+        var refreshToken = jwtTokenProvider.generateRefreshToken(user);
+
+        var cookie = new Cookie("refreshToken", refreshToken);
+        cookie.setHttpOnly(true);
+        cookie.setPath("/auth/refresh");
+        cookie.setMaxAge(jwtProperties.getRefreshTokenExpiration());
+        cookie.setSecure(true);
+        response.addCookie(cookie);
+
+        return ResponseEntity.ok(new JwtResponse(accessToken));
+    }
 }
