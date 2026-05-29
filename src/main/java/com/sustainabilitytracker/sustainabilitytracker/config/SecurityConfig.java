017e7fbe1fe9ff3bacfcae8b8d36830f08b399f4
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-05-29 16:37:25 +0300

    refactor: change the role route routes config

--- src/main/java/com/sustainabilitytracker/sustainabilitytracker/config/SecurityConfig.java
+++ src/main/java/com/sustainabilitytracker/sustainabilitytracker/config/SecurityConfig.java
@@ -62,7 +62,6 @@ public class SecurityConfig {
         return config.getAuthenticationManager();
     }
 
-
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
