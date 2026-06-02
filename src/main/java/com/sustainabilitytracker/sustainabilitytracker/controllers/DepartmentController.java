4115fede64ff7f2a7f81d15ed708c323c7b39766
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-06-03 01:26:41 +0300

    feat: add department controller.

---
+++ src/main/java/com/sustainabilitytracker/sustainabilitytracker/controllers/DepartmentController.java
@@ -0,0 +1,9 @@
+package com.sustainabilitytracker.sustainabilitytracker.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/department")
+public class DepartmentController {
+}
