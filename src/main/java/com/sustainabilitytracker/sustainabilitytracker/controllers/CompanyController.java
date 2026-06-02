0f095d5b564862b7a9ad97af8aade0430969319a
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-06-03 01:37:13 +0300

    feat: add create company controller method.

--- src/main/java/com/sustainabilitytracker/sustainabilitytracker/controllers/CompanyController.java
+++ src/main/java/com/sustainabilitytracker/sustainabilitytracker/controllers/CompanyController.java
@@ -1,13 +1,13 @@
 package com.sustainabilitytracker.sustainabilitytracker.controllers;
 
+import com.sustainabilitytracker.sustainabilitytracker.dtos.request.CompanyRequest;
 import com.sustainabilitytracker.sustainabilitytracker.dtos.response.CompanyResponse;
 import com.sustainabilitytracker.sustainabilitytracker.mappers.CompanyMapper;
 import com.sustainabilitytracker.sustainabilitytracker.services.CompanyService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
@@ -21,6 +21,17 @@ public class CompanyController {
     @GetMapping
     public List<CompanyResponse> getAllCompanies(){
        return companyService.getAllCompanies();
+    }
+
+    @PostMapping
+    public ResponseEntity<CompanyResponse> createCompany(
+            @RequestBody CompanyRequest request,
+            UriComponentsBuilder uriBuilder){
+
+        var companyResponse = companyService.createCompany(request);
+        var uri = uriBuilder.path("api/v1/company/{id}").buildAndExpand(companyResponse.getId()).toUri();
+
+       return ResponseEntity.created(uri).body(companyResponse);
     }
 }
 //         → getAllCompanies()
