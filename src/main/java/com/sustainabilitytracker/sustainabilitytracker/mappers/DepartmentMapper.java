1fac31e4257d6d3e14761aa7c887d13dcfc23625
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-06-03 04:29:19 +0300

    refactor: make code more readable

--- src/main/java/com/sustainabilitytracker/sustainabilitytracker/mappers/DepartmentMapper.java
+++ src/main/java/com/sustainabilitytracker/sustainabilitytracker/mappers/DepartmentMapper.java
@@ -4,9 +4,14 @@ import com.sustainabilitytracker.sustainabilitytracker.dtos.request.DepartmentRe
 import com.sustainabilitytracker.sustainabilitytracker.dtos.response.DepartmentResponse;
 import com.sustainabilitytracker.sustainabilitytracker.entities.Department;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface DepartmentMapper {
-    Department toEntity(DepartmentRequest departmentRequest);
+
+    Department toEntity(DepartmentRequest request);
+
+    @Mapping(target = "companyName", source = "company.name")
+    @Mapping(target = "createdAt", source = "createdAt")
     DepartmentResponse toResponse(Department department);
 }
