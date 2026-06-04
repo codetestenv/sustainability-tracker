afdf71a221759a1e3e5db78fb331778530595f22
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-06-04 18:19:36 +0300

    feat: Water response dto

--- src/main/java/com/sustainabilitytracker/sustainabilitytracker/dtos/response/WaterResponse.java
+++ src/main/java/com/sustainabilitytracker/sustainabilitytracker/dtos/response/WaterResponse.java
@@ -1,6 +1,5 @@
 package com.sustainabilitytracker.sustainabilitytracker.dtos.response;
 
-import com.sustainabilitytracker.sustainabilitytracker.enums.WaterSource;
 import lombok.Data;
 
 import java.math.BigDecimal;
