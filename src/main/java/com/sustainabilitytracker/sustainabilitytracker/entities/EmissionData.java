4ed52293549378a122c1d3215a8869d1f29e93f0
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-06-05 22:41:05 +0300

    refactor: add CreationTimestamp annotation for createdAt and submittedAt field

--- src/main/java/com/sustainabilitytracker/sustainabilitytracker/entities/EmissionData.java
+++ src/main/java/com/sustainabilitytracker/sustainabilitytracker/entities/EmissionData.java
@@ -6,6 +6,7 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.math.BigDecimal;
 import java.time.Instant;
@@ -66,14 +67,15 @@ public class EmissionData {
     @Column(name = "recorded_at")
     private LocalDate recordedAt;
 
-    @Column(name = "submitted_at")
+    @CreationTimestamp
+    @Column(name = "submitted_at", nullable = false, updatable = false)
     private Instant submittedAt;
 
     @Column(name = "approved_at")
     private Instant approvedAt;
 
-    @ColumnDefault("CURRENT_TIMESTAMP")
-    @Column(name = "created_at")
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
     private Instant createdAt;
 
 
