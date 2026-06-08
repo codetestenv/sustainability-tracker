2df1f79e9435adb48b1097a7a67a0b011090e98e
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-06-08 14:08:53 +0300

    refactor: add get emission by company id method

--- src/main/java/com/sustainabilitytracker/sustainabilitytracker/repositories/EmissionRepository.java
+++ src/main/java/com/sustainabilitytracker/sustainabilitytracker/repositories/EmissionRepository.java
@@ -1,12 +1,24 @@
 package com.sustainabilitytracker.sustainabilitytracker.repositories;
 
 
+import com.sustainabilitytracker.sustainabilitytracker.entities.Company;
+import com.sustainabilitytracker.sustainabilitytracker.entities.Department;
 import com.sustainabilitytracker.sustainabilitytracker.entities.EmissionData;
+import com.sustainabilitytracker.sustainabilitytracker.entities.User;
 import com.sustainabilitytracker.sustainabilitytracker.enums.DataStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public interface EmissionRepository extends JpaRepository<EmissionData, Long> {
     boolean existsByDepartmentIdAndRecordedAtAndStatus(Long departmentId, LocalDate attr0, DataStatus status);
+
+    List<EmissionData> findAllByCompany(Company company);
+
+    List<EmissionData> findAllBySubmittedBy(User submittedBy);
+
+    List<EmissionData> findAllByDepartment(Department department);
+
+    List<EmissionData> findAllByCompany_Id(Long companyId);
 }
