92be40845c71c15a915f7857164eeaf32f78d97c
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-06-09 15:01:47 +0300

    feat: Implement GET ENERGY SUMMARY method.

--- src/main/java/com/sustainabilitytracker/sustainabilitytracker/dtos/response/EnergySummaryResponse.java
+++ src/main/java/com/sustainabilitytracker/sustainabilitytracker/dtos/response/EnergySummaryResponse.java
@@ -1,10 +1,12 @@
 package com.sustainabilitytracker.sustainabilitytracker.dtos.response;
 
+import lombok.Builder;
 import lombok.Data;
 
 import java.math.BigDecimal;
 
 @Data
+@Builder
 public class EnergySummaryResponse {
     private BigDecimal totalKwh;
     private BigDecimal totalRenewableKwh;
