2656f2d75620f13de68d19bb8c87d2176b0de553
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-06-09 15:28:46 +0300

    feat: Implement SUBMIT ENERGY controller route.

---
+++ src/main/java/com/sustainabilitytracker/sustainabilitytracker/controllers/EnergyController.java
@@ -0,0 +1,43 @@
+package com.sustainabilitytracker.sustainabilitytracker.controllers;
+
+import com.sustainabilitytracker.sustainabilitytracker.dtos.request.EnergyRequest;
+import com.sustainabilitytracker.sustainabilitytracker.dtos.request.RejectRequest;
+import com.sustainabilitytracker.sustainabilitytracker.dtos.response.EnergyResponse;
+import com.sustainabilitytracker.sustainabilitytracker.dtos.response.EnergySummaryResponse;
+import com.sustainabilitytracker.sustainabilitytracker.repositories.EnergyRepository;
+import com.sustainabilitytracker.sustainabilitytracker.services.EnergyService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneOffset;
+import java.util.List;
+
+@AllArgsConstructor
+@Controller
+public class EnergyController {
+
+    private final EnergyService energyService;
+
+    // SUBMIT ENERGY
+    @PostMapping
+    public ResponseEntity<EnergyResponse> submitEnergy(
+            @Valid @RequestBody EnergyRequest energyRequest,
+            UriComponentsBuilder uriBuilder) {
+
+        EnergyResponse energyResponse = energyService.submitEnergy(energyRequest);
+
+        var uri = uriBuilder.path("/energy/{energyId}")
+                .buildAndExpand(energyResponse.getId())
+                .toUri();
+
+        return ResponseEntity.created(uri).body(energyResponse);
+    }
+
+
+}
