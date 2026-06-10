39fe419059c021f9f265e0360a13efc7ae710537
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-06-10 21:13:15 +0300

    refactor: add new feilds in to water response and change data type.

--- src/main/java/com/sustainabilitytracker/sustainabilitytracker/dtos/response/WaterResponse.java
+++ src/main/java/com/sustainabilitytracker/sustainabilitytracker/dtos/response/WaterResponse.java
@@ -1,5 +1,7 @@
 package com.sustainabilitytracker.sustainabilitytracker.dtos.response;
 
+import com.sustainabilitytracker.sustainabilitytracker.enums.DataStatus;
+import com.sustainabilitytracker.sustainabilitytracker.enums.WaterSource;
 import lombok.Data;
 
 import java.math.BigDecimal;
@@ -9,18 +11,19 @@ import java.time.LocalDate;
 @Data
 public class WaterResponse {
     private Long id;
+    private Long companyId;
     private String companyName;
-    private String submittedByName;
-    private String approvedByName;
-
+    private Long departmentId;
+    private String departmentName;
     private BigDecimal consumedLiters;
     private BigDecimal recycledLiters;
-    private String source;
-
-    private String status;
+    private WaterSource source;
+    private DataStatus status;
     private String notes;
     private String rejectionReason;
+    private LocalDate recordedAt;
     private Instant submittedAt;
     private Instant approvedAt;
-    private LocalDate recordedAt;
+    private String submittedByName;
+    private String approvedByName;
 }
