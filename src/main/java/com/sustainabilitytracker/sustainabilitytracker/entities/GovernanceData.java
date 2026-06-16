22bbfa1cda62a2bd6fe6db4874ca61d8828590fd
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-06-17 02:01:21 +0300

    refactor: upadte time.

--- src/main/java/com/sustainabilitytracker/sustainabilitytracker/entities/GovernanceData.java
+++ src/main/java/com/sustainabilitytracker/sustainabilitytracker/entities/GovernanceData.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.math.BigDecimal;
 import java.time.Instant;
@@ -70,9 +71,8 @@ public class GovernanceData {
     @Column(name = "approved_at")
     private Instant approvedAt;
 
-    @ColumnDefault("CURRENT_TIMESTAMP")
-    @Column(name = "created_at")
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
     private Instant createdAt;
-
 
 }
