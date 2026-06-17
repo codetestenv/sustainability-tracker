c925168bfd946dc3147cadd2e6edeaa1d3a097bb
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-06-17 15:11:04 +0300

    feat: add report service

--- src/main/java/com/sustainabilitytracker/sustainabilitytracker/controllers/ScoreCalculationController.java
+++ src/main/java/com/sustainabilitytracker/sustainabilitytracker/controllers/ScoreCalculationController.java
@@ -10,7 +10,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/scores")
+@RequestMapping("/scores")
 @RequiredArgsConstructor
 public class ScoreCalculationController {
 
