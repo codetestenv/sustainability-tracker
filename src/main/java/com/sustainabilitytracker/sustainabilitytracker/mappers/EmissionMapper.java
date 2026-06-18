b64c2d66a71e1f52b052246323f96042ddac9364
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-06-18 12:06:13 +0300

    fix(mappers): add , unmappedTargetPolicy = ReportingPolicy.IGNORE to all mapper to avoid ignored fields

--- src/main/java/com/sustainabilitytracker/sustainabilitytracker/mappers/EmissionMapper.java
+++ src/main/java/com/sustainabilitytracker/sustainabilitytracker/mappers/EmissionMapper.java
@@ -7,8 +7,9 @@ import com.sustainabilitytracker.sustainabilitytracker.entities.EmissionData;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
+import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface EmissionMapper {
 
     @Mapping(target = "id", ignore = true)
