55c14150a8418a591788871cb7c28fb3cdbab6a5
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-06-20 22:38:43 +0300

    refactor: change test class based service

--- src/test/java/com/sustainabilitytracker/sustainabilitytracker/ScoreCalculationServiceTest.java
+++ src/test/java/com/sustainabilitytracker/sustainabilitytracker/ScoreCalculationServiceTest.java
@@ -1,6 +1,7 @@
 package com.sustainabilitytracker.sustainabilitytracker;
 
 import com.sustainabilitytracker.sustainabilitytracker.entities.SustainabilityScore;
+import com.sustainabilitytracker.sustainabilitytracker.enums.PeriodType;
 import com.sustainabilitytracker.sustainabilitytracker.repositories.EmissionRepository;
 import com.sustainabilitytracker.sustainabilitytracker.repositories.EnergyRepository;
 import com.sustainabilitytracker.sustainabilitytracker.repositories.WaterRepository;
@@ -44,7 +45,8 @@ class ScoreCalculationServiceTest {
 
     @Test
     void calculateAndSaveScore_WithAllData_ShouldReturnCorrectScore() {
-        when(emissionRepository.getTotalCo2(anyLong(), any(), any())).thenReturn(BigDecimal.valueOf(850));
+        // Mock data
+        when(emissionRepository.getTotalCo2(anyLong(), any(), any())).thenReturn(BigDecimal.valueOf(800));
         when(energyRepository.getTotalKwh(anyLong(), any(), any())).thenReturn(BigDecimal.valueOf(5000));
         when(energyRepository.getTotalRenewableKwh(anyLong(), any(), any())).thenReturn(BigDecimal.valueOf(3500));
         when(waterRepository.getTotalConsumedLiters(anyLong(), any(), any())).thenReturn(BigDecimal.valueOf(15000));
@@ -52,12 +54,18 @@ class ScoreCalculationServiceTest {
         when(wasteRepository.getTotalKg(anyLong(), any(), any())).thenReturn(BigDecimal.valueOf(2000));
         when(wasteRepository.getTotalRecycledKg(anyLong(), any(), any())).thenReturn(BigDecimal.valueOf(1200));
 
+        when(scoreRepository.save(any(SustainabilityScore.class))).thenAnswer(i -> i.getArgument(0));
+
         SustainabilityScore score = scoreCalculationService
-                .calculateAndSaveScore(1L, LocalDate.now().minusMonths(1), LocalDate.now());
+                .calculateAndSaveScore(1L,
+                        LocalDate.now().minusMonths(1),
+                        LocalDate.now(),
+                        PeriodType.MONTHLY);
 
         assertNotNull(score);
         assertNotNull(score.getTotalScore());
         assertNotNull(score.getGrade());
+        assertEquals(PeriodType.MONTHLY, score.getPeriodType());
     }
 
     @Test
@@ -70,5 +78,11 @@ class ScoreCalculationServiceTest {
     void determineGrade_Score30_ReturnsF() {
         String grade = scoreCalculationService.determineGrade(30.0);
         assertEquals("F", grade);
+    }
+
+    @Test
+    void determineGrade_Score75_ReturnsB() {
+        String grade = scoreCalculationService.determineGrade(75.0);
+        assertEquals("B", grade);
     }
 }
