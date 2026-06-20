1534c7c3f5c1efd15c8d5a80b6f37ba7a124ac87
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-06-21 01:26:00 +0300

    refactor: change few line of code and repo and dtos

--- src/main/java/com/sustainabilitytracker/sustainabilitytracker/dtos/response/AdminDashboardResponse.java
+++ src/main/java/com/sustainabilitytracker/sustainabilitytracker/dtos/response/AdminDashboardResponse.java
@@ -1,24 +1,18 @@
 package com.sustainabilitytracker.sustainabilitytracker.dtos.response;
 
-import com.sustainabilitytracker.sustainabilitytracker.entities.Company;
 import com.sustainabilitytracker.sustainabilitytracker.entities.SustainabilityScore;
 import lombok.Builder;
 import lombok.Data;
-
-import java.util.List;
-import java.util.Map;
 
 @Data
 @Builder
 public class AdminDashboardResponse {
 
-    private Long totalCompanies;
-    private Long totalUsers;
-    private Long totalReports;
+    private long totalCompanies;
+    private long totalUsers;
 
     private SustainabilityScore bestScore;
     private SustainabilityScore worstScore;
 
-    private Map<String, Object> systemStats;
-    private List<Company> topCompanies;
+    private SystemStatsResponse stats;
 }
