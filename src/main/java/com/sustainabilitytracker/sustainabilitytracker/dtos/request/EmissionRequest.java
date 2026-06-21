35a5471d88101d6fdd78043fd6455b6c6c97e86b
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-06-22 01:11:08 +0300

    refactor: change few line of code and improve few fields

--- src/main/java/com/sustainabilitytracker/sustainabilitytracker/dtos/request/EmissionRequest.java
+++ src/main/java/com/sustainabilitytracker/sustainabilitytracker/dtos/request/EmissionRequest.java
@@ -1,15 +1,18 @@
 package com.sustainabilitytracker.sustainabilitytracker.dtos.request;
 
 import com.sustainabilitytracker.sustainabilitytracker.enums.EmissionScope;
-import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
-import java.time.Instant;
 import java.time.LocalDate;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class EmissionRequest {
     @NotNull
     private Long companyId;
@@ -17,14 +20,14 @@ public class EmissionRequest {
     @NotNull
     private Long departmentId;
 
-    @NotNull
-    @Min(0)
+    @NotNull(message = "CO2 amount is required")
+    @DecimalMin(value = "0.0", message = "CO2 amount cannot be negative")
     private BigDecimal co2Amount;
 
-    @Min(0)
+    @DecimalMin(value = "0.0", message = "CH4 amount cannot be negative")
     private BigDecimal ch4Amount;
 
-    @Min(0)
+    @DecimalMin(value = "0.0", message = "N2O amount cannot be negative")
     private BigDecimal n2oAmount;
 
     @NotNull
