35655abe1acf2c09c2b07e03c5c0f755d02aa75e
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-06-22 01:27:02 +0300

    refactor: change few line of code and improve few fields

--- src/main/java/com/sustainabilitytracker/sustainabilitytracker/dtos/response/ReportResponse.java
+++ src/main/java/com/sustainabilitytracker/sustainabilitytracker/dtos/response/ReportResponse.java
@@ -1,16 +1,20 @@
 package com.sustainabilitytracker.sustainabilitytracker.dtos.response;
 
+import com.sustainabilitytracker.sustainabilitytracker.enums.AuditStatus;
 import com.sustainabilitytracker.sustainabilitytracker.enums.ReportType;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.Instant;
 import java.time.LocalDate;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ReportResponse {
-
     private Long id;
     private Long companyId;
     private String companyName;
@@ -19,6 +23,7 @@ public class ReportResponse {
     private ReportType reportType;
     private String fileFormat;
     private String downloadUrl;
+    private AuditStatus auditStatus;
     private LocalDate periodStart;
     private LocalDate periodEnd;
     private Instant generatedAt;
