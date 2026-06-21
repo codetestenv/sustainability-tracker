24be809d408fd1680b334397daa0fbffc6f28381
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-06-22 01:41:12 +0300

    refactor(mappers): change few line of code and improve few fields

--- src/main/java/com/sustainabilitytracker/sustainabilitytracker/mappers/GovernanceMapper.java
+++ src/main/java/com/sustainabilitytracker/sustainabilitytracker/mappers/GovernanceMapper.java
@@ -7,22 +7,31 @@ import org.mapstruct.*;
 
 import java.util.List;
 
-@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+@Mapper(componentModel = "spring",
+        unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface GovernanceMapper {
 
-    @Mapping(target = "company", ignore = true)
-    @Mapping(target = "submittedBy", ignore = true)
-    @Mapping(target = "approvedBy", ignore = true)
+    @Mapping(target = "company",         ignore = true)
+    @Mapping(target = "submittedBy",     ignore = true)
+    @Mapping(target = "approvedBy",      ignore = true)
+    @Mapping(target = "status",          ignore = true)
+    @Mapping(target = "submittedAt",     ignore = true)
+    @Mapping(target = "approvedAt",      ignore = true)
+    @Mapping(target = "createdAt",       ignore = true)
+    @Mapping(target = "rejectionReason", ignore = true)
     GovernanceData toEntity(GovernanceRequest request);
 
-    @Mapping(target = "companyId", source = "company.id")
-    @Mapping(target = "companyName", source = "company.name")
+    @Mapping(target = "companyId",       source = "company.id")
+    @Mapping(target = "companyName",     source = "company.name")
     @Mapping(target = "submittedByName", source = "submittedBy.fullName")
-    @Mapping(target = "approvedByName", source = "approvedBy.fullName")
+    @Mapping(target = "approvedByName",  source = "approvedBy.fullName")
     GovernanceResponse toResponse(GovernanceData governanceData);
 
-    List<GovernanceResponse> toResponseList(List<GovernanceData> governanceDataList);
+    List<GovernanceResponse> toResponseList(
+            List<GovernanceData> governanceDataList);
 
-    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
-    void updateEntity(@MappingTarget GovernanceData governanceData, GovernanceRequest request);
+    @BeanMapping(nullValuePropertyMappingStrategy =
+            NullValuePropertyMappingStrategy.IGNORE)
+    void updateEntity(@MappingTarget GovernanceData governanceData,
+                      GovernanceRequest request);
 }
