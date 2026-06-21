6888a0fa27e6404f34f2a16a01fe76a2d7f49920
Eyob Mulugeta <eyobbmulugeta@gmail.com>
2026-06-22 01:41:00 +0300

    refactor(mappers): change few line of code and improve few fields

--- src/main/java/com/sustainabilitytracker/sustainabilitytracker/mappers/CompanyMapper.java
+++ src/main/java/com/sustainabilitytracker/sustainabilitytracker/mappers/CompanyMapper.java
@@ -3,14 +3,20 @@ package com.sustainabilitytracker.sustainabilitytracker.mappers;
 import com.sustainabilitytracker.sustainabilitytracker.dtos.request.CompanyRequest;
 import com.sustainabilitytracker.sustainabilitytracker.dtos.response.CompanyResponse;
 import com.sustainabilitytracker.sustainabilitytracker.entities.Company;
-import org.mapstruct.Mapper;
-import org.mapstruct.MappingTarget;
-import org.mapstruct.ReportingPolicy;
+import org.mapstruct.*;
 
-@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+@Mapper(componentModel = "spring",
+        unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface CompanyMapper {
+
     Company toEntity(CompanyRequest companyRequest);
 
     CompanyResponse toResponse(Company company);
-    void update(CompanyRequest companyRequest, @MappingTarget Company company);
+
+    @BeanMapping(nullValuePropertyMappingStrategy =
+            NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "isActive",  ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    void update(CompanyRequest companyRequest,
+                @MappingTarget Company company);
 }
